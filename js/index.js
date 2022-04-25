9fe0a4252f08a3381b5a84cd9806d0ef2b6b1f71
maggiew98 <100744382+maggiew98@users.noreply.github.com>
2022-04-25 10:17:10 -0500

    hw 19

---
+++ js/index.js
@@ -0,0 +1,11 @@
+console.log ("your index.js file is loaded correctly");
+
+
+$("#myID").on("click", function(){ console.log("you clicked on #myID");
+});
+
+
+
+btn.k('click', e => { 
+    console.log(e)
+})
